4a7ff692422c202a81ab43a7aeae13d6e90317c5
David Sanabria <dc_sanabria@yahoo.com>
2022-02-12 18:52:46 -0500

    added lights on for play

--- index.js
+++ index.js
@@ -8,10 +8,29 @@ var upload = multer({ dest: '/tmp/' });
 
 app.post('/', upload.single('thumb'), function (req, res, next) {
     var payload = JSON.parse(req.body.payload);
-    console.log('Got webhook for', payload.event);
+    console.log('Got webhook for', payload.event, '\nOn device', payload.device);
 
-    if (payload.Player.uuid = player && payload.event == "media.play" && payload.Metadata.type == "track") {
+    if (payload.Player.uuid === player && payload.event === "media.play") {
         console.log('Playing track', payload.Metadata.name);
+        var url = "192.168.5.12";
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", url);
+
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                console.log(xhr.responseText);
+            }
+        };
+
+        var data = `{
+            "on": "t",
+        }`;
+        console.log('Sending data to WLED');
+        xhr.send(data);
     }
 
     res.sendStatus(200);
